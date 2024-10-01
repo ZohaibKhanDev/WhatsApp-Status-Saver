7396268533fc697c38f62310818809b130e29c61
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-10-01 13:13:46 +0500

    Bug Fix

--- app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/Whatsapp Status Screen.kt
+++ app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/Whatsapp Status Screen.kt
@@ -1,8 +1,14 @@
 package com.example.whatsappstatus_saver.presentation.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
@@ -10,7 +16,8 @@ import com.google.accompanist.permissions.rememberPermissionState
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun WhatsAppStatusScreen() {
-    val permissionState = rememberPermissionState(permission = android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    val permissionState =
+        rememberPermissionState(permission = android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
     LaunchedEffect(Unit) {
         permissionState.launchPermissionRequest()
@@ -19,6 +26,12 @@ fun WhatsAppStatusScreen() {
     if (permissionState.status.isGranted) {
         DisplayWhatsAppStatuses()
     } else {
-        Text("Please grant storage access to view WhatsApp statuses.")
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(
+                "Please grant storage access to view WhatsApp statuses.",
+                fontSize = 21.sp,
+                fontWeight = FontWeight.Medium,
+            )
+        }
     }
 }
