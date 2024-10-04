0aab939abb563296d194ab362a6aa2cbca509805
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-10-04 15:06:05 +0500

    Ui Enhance

--- app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/screens/PicDetail.kt
+++ app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/screens/PicDetail.kt
@@ -5,12 +5,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 
 @Composable
 fun PicDetail(navController: NavController, PicPath: String) {
-
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        AsyncImage(
+            model = PicPath,
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
