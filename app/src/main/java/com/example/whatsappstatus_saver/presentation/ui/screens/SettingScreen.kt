a8325a24e17fcafdb60079d5eefd783952f416b3
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-10-05 16:10:03 +0500

    Ui Enhance

--- app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/screens/SettingScreen.kt
+++ app/src/main/java/com/example/whatsappstatus_saver/presentation/ui/screens/SettingScreen.kt
@@ -1,16 +1,139 @@
 package com.example.whatsappstatus_saver.presentation.ui.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.Language
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SettingScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Coming Soon")
+
+
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(
+                text = "Setting",
+                color = Color.White,
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp
+            )
+        }, navigationIcon = {
+            Icon(
+                imageVector = Icons.Outlined.ArrowBack,
+                contentDescription = "",
+                tint = Color.White
+            )
+        }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0XFF008069)))
+    }) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 80.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(start = 30.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(30.dp)
+                        .height(30.dp)
+                        .border(
+                            BorderStroke(1.dp, color = Color.LightGray),
+                            shape = RoundedCornerShape(6.dp)
+                        ), contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Language,
+                        contentDescription = "",
+                        tint = Color.Gray, modifier = Modifier.size(21.dp)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier.wrapContentHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = "Languages",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.DarkGray
+                    )
+
+                    Text(
+                        text = "Default {English}",
+                        color = Color.LightGray,
+                        fontSize = 11.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+
+
+                Icon(
+                    imageVector = Icons.Outlined.ArrowBackIosNew,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(start = 20.dp)
+                        .rotate(180f)
+                        .weight(1f)
+                        .align(Alignment.CenterVertically), tint = Color.LightGray
+                )
+            }
+
+
+
+            Text(
+                text = "Others",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 30.dp), color = Color(0XFF008069)
+            )
+
+        }
     }
+
 }
