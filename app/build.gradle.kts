ed8c55e3eeccefd76587b246746007cdad867b7b
Muhammad Zohaib Imtiaz <159874349+ZohaibKhanDev@users.noreply.github.com>
2024-10-07 08:58:38 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.example.whatsappstatus_saver"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 1 
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
